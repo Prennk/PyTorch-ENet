891230a8b6d75465b733b677a0a43f8c0f5ee7c3
Affan Ardana <112737543+Prennk@users.noreply.github.com>
2024-08-10 12:09:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -94,9 +94,9 @@ def load_dataset(dataset):
 
     # Get a batch of samples to display
     if args.mode.lower() == 'test':
-        images, labels = iter(test_loader).next()
+        images, labels = next(iter(test_loader))
     else:
-        images, labels = iter(train_loader).next()
+        images, labels = next(iter(train_loader))
     print("Image size:", images.size())
     print("Label size:", labels.size())
     print("Class-color encoding:", class_encoding)
@@ -248,7 +248,7 @@ def test(model, test_loader, class_weights, class_encoding):
     # Show a batch of samples and labels
     if args.imshow_batch:
         print("A batch of predictions from the test set...")
-        images, _ = iter(test_loader).next()
+        images, _ = next(iter(test_loader))
         predict(model, images, class_encoding)
 
 
